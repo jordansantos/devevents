00e5cb5d1ae6dacd1b1bf9673235c6e52d128b3b
jordansantos <jordansant@gmail.com>
2021-11-22 21:10:38 -0300

    Criação das entidades do projeto e o esqueleto do controller de categoria;

---
+++ DevEvents.API/Controllers/CategoriaController.cs
@@ -0,0 +1,44 @@
+﻿using DevEvents.API.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevEvents.API.Controllers
+{
+  [ApiController]
+  [Route("[categoria]")]
+  public class CategoriaController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult obterTodos()
+    {
+      return Ok("Chamando Método Corretamente!!!");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ObterCategoria(int id)
+    {
+      return NotFound();
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar([FromBody] Categoria categoria)
+    {
+      return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Atualizar(int id, [FromBody] Categoria categoria)
+    {
+      return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Deletar(int id)
+    {
+      return Ok();
+    }
+  }
+}
